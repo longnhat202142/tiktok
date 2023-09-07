dc0e6c037ba8cc1eb75726948b4896589ab288d8
LongNhat <longnhat03082002@gmail.com>
2023-09-07 11:48:33 +0700

    Xây dựng Header Tiktok phần 1

---
+++ src/pages/Upload/index.js
@@ -0,0 +1,13 @@
+import Header from '~/components/Layout/components/Header';
+function Upload() {
+    return (
+        <div>
+            <Header />
+            <div className="content">
+                <h2>Upload Page</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Upload;
